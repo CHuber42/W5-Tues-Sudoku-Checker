9fdec472c4fa6018ce5c8891d9f7d3e4392cd4bc
Christopher Huber <CHuber42.work@gmail.com>
2020-04-14 11:52:08 -0700

    remove redundant css import from main that was sabotaging tests

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import $ from 'jquery';
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+export let aSudokuBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9,
+  2, 3, 4, 5, 6, 7, 8, 9, 1, 
+  3, 4, 5, 6, 7, 8, 9, 1, 2, 
+  4, 5, 6, 7, 8, 9, 1, 2, 3,
+  5, 6 ,7, 8, 9, 1, 2, 3, 4,
+  6, 7, 8, 9, 1, 2, 3, 4, 5,
+  7, 8, 9, 1, 2, 3, 4, 5, 6,
+  8, 9, 1, 2, 3, 4, 5, 6, 7,
+  9, 1, 2, 3, 4, 5, 6, 7, 8];
+
+export let valuesArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
