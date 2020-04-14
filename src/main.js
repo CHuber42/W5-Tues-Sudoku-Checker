b000d403f96164329952e47b8bb1a4883e7fa50e
Christopher Huber <CHuber42.work@gmail.com>
2020-04-14 16:08:36 -0700

    add js/html to gather sudoku board inputs and build board

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
-//import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import './styles.css';
 
-export let aSudokuBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9,
-                           2, 3, 4, 5, 6, 7, 8, 9, 1, 
-                           3, 4, 5, 6, 7, 8, 9, 1, 2, 
-                           4, 5, 6, 7, 8, 9, 1, 2, 3,
-                           5, 6 ,7, 8, 9, 1, 2, 3, 4,
-                           6, 7, 8, 9, 1, 2, 3, 4, 5,
-                           7, 8, 9, 1, 2, 3, 4, 5, 6,
-                           8, 9, 1, 2, 3, 4, 5, 6, 7,
-                           9, 1, 2, 3, 4, 5, 6, 7, 8];
+// use this for tests
+// export let aSudokuBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9,
+//                            2, 3, 4, 5, 6, 7, 8, 9, 1, 
+//                            3, 4, 5, 6, 7, 8, 9, 1, 2, 
+//                            4, 5, 6, 7, 8, 9, 1, 2, 3,
+//                            5, 6 ,7, 8, 9, 1, 2, 3, 4,
+//                            6, 7, 8, 9, 1, 2, 3, 4, 5,
+//                            7, 8, 9, 1, 2, 3, 4, 5, 6,
+//                            8, 9, 1, 2, 3, 4, 5, 6, 7,
+//                            9, 1, 2, 3, 4, 5, 6, 7, 8];
+
+let aSudokuBoard = [];
 
  
 export let valuesArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -67,3 +70,19 @@ export function checkFirstColumn() {
     }
   }
 }
+
+$("button").click(function(event) {
+  event.preventDefault();
+  $("input").each(function() {
+    aSudokuBoard.push(parseInt($(this).val()));
+  });
+  let columnLegal = checkFirstColumn();
+  let rowLegal = checkFirstRow();
+  if ((rowLegal == "Legal!") && (columnLegal == "Legal!")) {
+    //print legal
+  }
+  else {
+    //print illegal
+  }
+});
+  
