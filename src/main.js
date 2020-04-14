de9d9ff4676d0260a6dfde5cf4b97daad7e2b456
Christopher Huber <CHuber42.work@gmail.com>
2020-04-14 13:12:54 -0700

    add js + tests to check first row; currently failing - always passes test even if illegal board

--- src/main.js
+++ src/main.js
@@ -3,13 +3,39 @@ import 'bootstrap';
 import './styles.css';
 
 export let aSudokuBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9,
-  2, 3, 4, 5, 6, 7, 8, 9, 1, 
-  3, 4, 5, 6, 7, 8, 9, 1, 2, 
-  4, 5, 6, 7, 8, 9, 1, 2, 3,
-  5, 6 ,7, 8, 9, 1, 2, 3, 4,
-  6, 7, 8, 9, 1, 2, 3, 4, 5,
-  7, 8, 9, 1, 2, 3, 4, 5, 6,
-  8, 9, 1, 2, 3, 4, 5, 6, 7,
-  9, 1, 2, 3, 4, 5, 6, 7, 8];
-
+                           2, 3, 4, 5, 6, 7, 8, 9, 1, 
+                           3, 4, 5, 6, 7, 8, 9, 1, 2, 
+                           4, 5, 6, 7, 8, 9, 1, 2, 3,
+                           5, 6 ,7, 8, 9, 1, 2, 3, 4,
+                           6, 7, 8, 9, 1, 2, 3, 4, 5,
+                           7, 8, 9, 1, 2, 3, 4, 5, 6,
+                           8, 9, 1, 2, 3, 4, 5, 6, 7,
+                           9, 1, 2, 3, 4, 5, 6, 7, 8];
+ 
 export let valuesArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export function checkFirstRow() {
+  for (let i = 0; i < 9; i++){
+    let checking_num = aSudokuBoard[i];
+    loopInner:
+    for (let j = 0; j < 9; j++) {
+      if (valuesArray[j] == checking_num) {
+        valuesArray.splice(j, 1);
+        break loopInner;
+      }
+    }
+  }
+}
+
+// export function checkFirstColumn() {
+//   for (let i = 0; i < 9; i++){
+//     let checking_num = aSudokuBoard[i*9];
+//     loopInner:
+//     for (let j = 0; j < 9; j++) {
+//       if (valuesArray[j] == checking_num) {
+//         valuesArray.splice(j, 1);
+//         break loopInner;
+//       }
+//     }
+//   }
+// }
